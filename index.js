f8136419306632f29e94ec1200bec7dea92289bb
sudheerkosanam <kosanamsaisudheer@gmail.com>
2018-03-19 16:26:57 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+var express = require("express");
+var app = express();
+var port = 3000;
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/node-mongodb");
+ 
+var nameSchema = new mongoose.Schema({
+ firstName: String,
+ lastName: String
+});
+var User = mongoose.model("User", nameSchema);
+
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/view.html");
+});
+
+app.post("/addname", (req, res) => {
+    var myData = new User(req.body);
+    myData.save()
+       .then(item =>{
+       	res.send("Name saved to database")
+       })
+       .catch(err =>{
+       	res.status(400).send("Unable to save database");
+       })
+});
+
+// app.get("/", (req, res) => {
+//  res.send("Hello World");
+// });
+ 
+app.listen(port, () => {
+ console.log("Server listening on port " + port);
+});
